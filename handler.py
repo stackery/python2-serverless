2f2dd10264a1813a2024d1a6c374c78de24018b5
Anna Spysz <anna.spysz@stackery.io>
2019-06-03 15:14:25 -0700

    Modified function

--- handler.py
+++ handler.py
@@ -1,11 +1,6 @@
-import datetime
-import logging
+import json
+def handler(event, context):
+    # Log the event argument for debugging and for use in local development.
+    print(json.dumps(event))
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-
-def run(event, context):
-    current_time = datetime.datetime.now().time()
-    name = context.function_name
-    logger.info("Your cron function " + name + " ran at " + str(current_time))
+    return {}
